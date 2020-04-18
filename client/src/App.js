67c2e05ef1fdf5abce6fb70ef7a62bb40f922952
Adil Naimi <adilnm23@gmail.com>
2020-04-17 20:38:44 -0500

    change the App component to a class

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Signup from './Signup';
 import Login from './Login';
 import Home from './Home';
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
-      <Route exact path="/signup" component={Signup} />
-      <Route exact path="/login" component={Login} />
-      <Route exact path="/" component={Home} />
-      </Router>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/" component={Home} />
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;

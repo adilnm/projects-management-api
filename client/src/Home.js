dd7b22db1def2d9170a3895b1ab72f13b74bf0ba
Adil Naimi <adilnm23@gmail.com>
2020-04-18 19:03:04 -0500

    create a notification box

--- client/src/Home.js
+++ client/src/Home.js
@@ -9,6 +9,8 @@ class Home extends Component {
         this.props.projects()
     }
     render() {
+        const{allProjects}=this.props
+        const l=allProjects.length
         return (
             <div className='row'>
                 <div className="col-md-6 text-center">
@@ -17,9 +19,11 @@ class Home extends Component {
                 <div className="col-md-6 text-center">
                     <div className="card" >
                         <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <h5 className="card-title">Notifications</h5>
+                            {allProjects.slice(l-3,l).map(project=>{
+                               return  <p className="card-text">{project.user.name} added a new project</p>
+                            })}
+
                         </div>
                     </div>
                 </div>
--- client/src/ProjectsList.js
+++ client/src/ProjectsList.js
@@ -1,12 +1,12 @@
 import React from 'react'
+import {Link} from 'react-router-dom';
 
 export default function projectsList(props) {
     const { project } = props
     return (
         <div className="card" >
             <div className="card-body">
-                <h5 className="card-title">{project.title}</h5>
-                {/* <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6> */}
+            <Link to="/"><h5 className="card-title">{project.title}</h5></Link>
                 <p className="card-text">posted by {project.user.name}</p>
                 <div className="card-footer text-muted">
                     {project.created_at}

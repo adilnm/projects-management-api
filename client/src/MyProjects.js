5d61b0d0e8696dbdae6e86e2c10742e50ad3c2ea
Adil Naimi <adilnm23@gmail.com>
2020-05-07 22:34:03 -0500

    create a component to show the user projects only

---
+++ client/src/MyProjects.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class MyProjects extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}

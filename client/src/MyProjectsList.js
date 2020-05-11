7299861b7aa88acfc5672149feefdaddbc358725
Adil Naimi <adilnm23@gmail.com>
2020-05-10 23:50:19 -0500

    switch MyProjectsLists to a class component

--- client/src/MyProjectsList.js
+++ client/src/MyProjectsList.js
@@ -1,17 +1,21 @@
-import React from 'react'
+import React, { Component } from 'react'
 import {Link} from 'react-router-dom';
 
-export default function MyProjectList(props) {
-    const { project } = props
-    return (
-        <div className="card" >
-            <div className="card-body">
-            <Link to={`/projects/${project.id}`}><h5 className="card-title">{project.title}</h5></Link>
-                <p className="card-text">posted by {project.user.name}</p>
-                <div className="card-footer text-muted">
-                    {project.created_at}
+export default class extends Component {
+
+    render(){
+        const { project } = this.props
+        return (
+            <div className="card" >
+                <div className="card-body">
+                <Link to={`/projects/${project.id}`}><h5 className="card-title">{project.title}</h5></Link>
+                    <p className="card-text">posted by {project.user.name}</p>
+                    <button onClick={this.handleDelet}>Delete</button>
+                    <div className="card-footer text-muted">
+                        {project.created_at}
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }

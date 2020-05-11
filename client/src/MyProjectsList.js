da0ce93f1de9acb09c0bdde87b6017ecd24328fd
Adil Naimi <adilnm23@gmail.com>
2020-05-10 23:52:16 -0500

    create handleDelete method in order to fetch a delete request to the api

--- client/src/MyProjectsList.js
+++ client/src/MyProjectsList.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react'
 import {Link} from 'react-router-dom';
 
-export default class extends Component {
+export default class MyProjectsLists extends Component {
 
+    handleDelete=(e)=>{
+        
+    }
     render(){
         const { project } = this.props
         return (
@@ -10,7 +13,7 @@ export default class extends Component {
                 <div className="card-body">
                 <Link to={`/projects/${project.id}`}><h5 className="card-title">{project.title}</h5></Link>
                     <p className="card-text">posted by {project.user.name}</p>
-                    <button onClick={this.handleDelet}>Delete</button>
+                    <button onClick={this.handleDelete}>Delete</button>
                     <div className="card-footer text-muted">
                         {project.created_at}
                     </div>
